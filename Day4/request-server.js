a48281798fb0b01eba38416ff73ebfb05563d67d
mstars_lab2_11 <lab_02_01@Lab-02-01s-iMac.local>
2022-04-15 17:43:51 +0800

    question bla2

--- Day4/request-server.js
+++ Day4/request-server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var fs = require('fs');
+var queryString = require('querystring')
 img = {
   'Content-Type': 'image/jpg'
 }
@@ -37,8 +38,38 @@ http
         response.write('<h1>Mp3 edr haahah</h1>')
         response.end();
       }
+      else if(request.url === '/q?a=5&x=57&y=3&z=1'){
+        const {a,x,y,z} = queryString.parse(request.url.slice(3))
+
+        response.writeHead(200, txt);
+        const total = Number(a)+Number(y)+Number(x)+Number(z)
+        console.log(typeof total);
+        response.write(`${total}`)
+        response.end();
+      }
+      else if(request.url.slice(0, 11) === '/q?question'){
+        //   console.log('request.url');
+        // const {a,x,y,z} = queryString.parse(request.url.slice(3))
+        response.writeHead(200, txt);
+        if(request.url.slice(12) == 'why'){
+            response.write(`doesnt match`)
+            response.end();
+        }
+        if(request.url.slice(12) == 'hi'){
+            response.write(`bye`)
+            response.end();
+        }
+        else{
+            response.write(`mori`)
+            response.end();
+        }
+        // const total = Number(a)+Number(y)+Number(x)+Number(z)
+        // console.log(typeof total);
+        
+      }
 
       else{
+          console.log(typeof '/q?question'.slice(0, 11));
           response.writeHead(404, txt);
           response.write("Not Found.")
           response.end();
