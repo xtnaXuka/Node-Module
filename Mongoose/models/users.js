0cdee3a38ceb989d032ddd6109badf179834d228
mstars_lab2_11 <lab_02_01@Lab-02-01s-iMac.local>
2022-06-05 18:16:24 +0800

    a

--- Mongoose/models/users.js
+++ Mongoose/models/users.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const pollSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  pollName: Number,
+  orderdate:String,
+  orderNumber: Number,
+  user: String,
+  order: String,
+  total: String,
+  card: String,
+  phoneNumber: Number,
 });
 module.exports = mongoose.model("Poll", pollSchema);
